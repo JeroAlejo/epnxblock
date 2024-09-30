5a5682417eb0e7d2d455dad7be4a6b2088e5d94e
JeroAlejo <jere_mpm@hotmail.com>
2024-09-30 15:03:32 -0500

    Cambio: Paso de infromacion 2 Vistas

--- epnxblock/epnxblock.py
+++ epnxblock/epnxblock.py
@@ -9,7 +9,7 @@ import logging
 import pkg_resources
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock, XBlockAside
-from xblock.fields import Integer, Scope
+from xblock.fields import Integer, Scope, String
 
 log = logging.getLogger(__name__)
 
@@ -40,13 +40,32 @@ class EpnXBlock(XBlock):
         scope = Scope.user_state
     )
 
+    #Campo para almacenar informacion: PRUEBA ------------------------
+    titulo = String(
+        help="Titulo de la arctividad",
+        default=None,
+        scope = Scope.user_state
+    )
+
+    descripcion = String(
+        help="Descripcion de la arctividad",
+        default=None,
+        scope = Scope.user_state
+    )
+
+    fecha_entrega = String(
+        help="Fecha de la arctividad",
+        default=None,
+        scope = Scope.user_state
+    )
+
     def resource_string(self, path):
         """Ayudante práctico para obtener recursos de nuestro kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    # Vista del studiante
-    def student_view(self, context=None):
+    #Vista de profesor
+    def studio_view(self, context = None):
         """
         La vista principal de EpnXBlock, que se muestra a los estudiantes
         cuando visualizan cursos.
@@ -54,26 +73,46 @@ class EpnXBlock(XBlock):
         #Carga de Fragmento HTML
         html_str = importlib.resources.files(__package__).joinpath("static/html/epnxblock.html").read_text(encoding="utf-8")
         frag = Fragment(str(html_str).format(block=self))
+         # Carga de archivos CSS y JavaScript fragments from within the package
+        css_str = importlib.resources.files(__package__).joinpath("static/css/epnxblock.css").read_text(encoding="utf-8")
+        frag.add_css(str(css_str))
+        js_str = importlib.resources.files(__package__).joinpath("static/js/src/epnxblock.js").read_text(encoding="utf-8")
+        frag.add_javascript(str(js_str))
+        frag.initialize_js('Profesor_View')
+
+        return frag
+
+    # Vista del estudiante -- PRUEBA 
+    def student_view(self, context=None):
+        """
+        La vista principal de EpnXBlock, que se muestra a los estudiantes
+        cuando visualizan cursos.
+        """
+        
+        #Carga de Fragmento HTML
+        html_str = importlib.resources.files(__package__).joinpath("static/html/epnxblock_student.html").read_text(encoding="utf-8")
+        frag = Fragment(str(html_str).format(block=self))
 
         # Carga de archivos CSS y JavaScript fragments from within the package
-        css_str = importlib.resources.files(__package__).joinpath("static/css/epnxblock.css").read_text(encoding="utf-8")
+        css_str = importlib.resources.files(__package__).joinpath("static/css/epnxblock_student.css").read_text(encoding="utf-8")
         frag.add_css(str(css_str))
 
         js_str = importlib.resources.files(__package__).joinpath("static/js/src/epnxblock.js").read_text(encoding="utf-8")
         frag.add_javascript(str(js_str))
         frag.initialize_js('EpnXBlock')
 
-        #POR ELIMINAR
-        """
-        html = self.resource_string("static/html/epnxblock.html")
-        frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/epnxblock.css"))
-        frag.add_javascript(self.resource_string("static/js/src/epnxblock.js"))
-        
-        """
         return frag
     
-    problem_view = student_view
+    
+    
+
+
+    # Método para manejar los datos que se pasan entre las vistas
+    @XBlock.json_handler
+    def save_shared_data(self, data, suffix=''):
+        """Handler para guardar datos compartidos"""
+        self.shared_data = data.get('shared_data')
+        return {"result": "success"}
 
     #CONTROLADORES 
     
@@ -100,6 +139,18 @@ class EpnXBlock(XBlock):
         #Retorna un diccionario
         return {'up': self.upvotes, 'down': self.downvotes}
 
+
+    @XBlock.json_handler
+    def guardar_configuracion(self, data, suffix=''):
+        "Manejador que guarda las configuraciones del profesor en los campos"
+        self.titulo = data.get('titulo')
+        self.descripcion = data.get('descripcion')
+        self.fecha_entrega = data.get('fecha')
+        
+        return {
+            'result':'succes',
+            'message':'Datos guardados correctament'
+        }
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
     @staticmethod
@@ -109,11 +160,7 @@ class EpnXBlock(XBlock):
             ("EpnXBlock",
              """<epnxblock/>
              """),
-            ("Multiple EpnXBlock",
-             """<vertical_demo>
-                <epnxblock/>
-                <epnxblock/>
-                <epnxblock/>
-                </vertical_demo>
-             """),
+            
         ]
+    
+    
